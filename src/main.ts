8ca75962beb858697ac373eb9bce59e975981f0b
oshersa-170701 <oshersa1771@gmail.com>
2026-08-01 22:51:04 -0600

    Cambio B5

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { Request, Response, NextFunction } from 'express';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
   app.enableCors({ origin: true });
-  app.setGlobalPrefix('api'); // <--- ESTO EVITA QUE EXPRESS SE PELEE CON LAS RUTAS DE ANGULAR
+  app.setGlobalPrefix('api');
 
   app.useStaticAssets(join(__dirname, '..', 'www'));
 
-  app.use((req, res, next) => {
+  app.use((req: Request, res: Response, next: NextFunction) => {
     if (req.path.startsWith('/api') || req.path.includes('.')) {
       return next();
     }
