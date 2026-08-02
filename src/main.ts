59f418d74e1b412841153576cb75f8c04f94bfbd
oshersa-170701 <oshersa1771@gmail.com>
2026-08-01 23:06:55 -0600

    Cambio B6

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  
+
   app.enableCors({ origin: true });
-  app.setGlobalPrefix('api');
 
+  // Sirve los archivos estáticos de Ionic directamente
   app.useStaticAssets(join(__dirname, '..', 'www'));
-
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    if (req.path.startsWith('/api') || req.path.includes('.')) {
-      return next();
-    }
-    res.sendFile(join(__dirname, '..', 'www', 'index.html'));
-  });
 
   await app.listen(process.env.PORT || 3000);
 }
--- www/chunk-EHHF3BM2.js
+++
@@ -1,1 +0,0 @@
-var p={production:!0,apiUrl:"https://bot-wa-back-production.up.railway.app/api"};export{p as a};
