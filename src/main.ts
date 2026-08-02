d952d3a014ec2e0c4490397c75d2c9af95f508bc
oshersa-170701 <oshersa1771@gmail.com>
2026-08-01 22:43:18 -0600

    Cambio B4

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
   app.enableCors({ origin: true });
+
+  // Sirve los archivos estáticos de Ionic directamente
   app.useStaticAssets(join(__dirname, '..', 'www'));
 
+  // Redirige cualquier ruta que no sea de la API hacia el index.html de Ionic (para el SPA router)
   app.use((req, res, next) => {
-    if (
-      req.path.startsWith('/users') || 
-      req.path.startsWith('/login') || // <--- EXCLUIMOS EXPLÍCITAMENTE /LOGIN AQUÍ
-      req.path.includes('.')
-    ) {
+    if (req.path.startsWith('/users') || req.path.includes('.')) {
       return next();
     }
     res.sendFile(join(__dirname, '..', 'www', 'index.html'));
