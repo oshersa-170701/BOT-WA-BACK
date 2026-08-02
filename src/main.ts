ef0403d8705ccfda2b43cbd6f0156d76b8e3d850
oshersa-170701 <oshersa1771@gmail.com>
2026-08-01 22:47:09 -0600

    Cambio B4

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
-
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
   app.enableCors({ origin: true });
+  app.setGlobalPrefix('api'); // <--- ESTO EVITA QUE EXPRESS SE PELEE CON LAS RUTAS DE ANGULAR
 
-  // Sirve los archivos estáticos de Ionic directamente
   app.useStaticAssets(join(__dirname, '..', 'www'));
 
-  // Redirige cualquier ruta que no sea de la API hacia el index.html de Ionic (para el SPA router)
   app.use((req, res, next) => {
-    if (req.path.startsWith('/users') || req.path.includes('.')) {
+    if (req.path.startsWith('/api') || req.path.includes('.')) {
       return next();
     }
     res.sendFile(join(__dirname, '..', 'www', 'index.html'));
